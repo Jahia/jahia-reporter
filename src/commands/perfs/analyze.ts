47f4d0ed9006303a63e26d38a412c189b50ee558
Francois G <fgerthoffert@jahia.com>
2023-05-09 13:08:49 +0200

    Removed changes in perf commands

--- src/commands/perfs/analyze.ts
+++ src/commands/perfs/analyze.ts
@@ -1,9 +1,9 @@
 /* eslint-disable complexity */
 /* eslint-disable max-depth */
-import {Command, flags} from '@oclif/command'
-import {readFileSync} from 'fs'
-import * as fs from 'fs'
-import * as path from 'path'
+import { Command, flags } from '@oclif/command';
+import { readFileSync } from 'fs';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import {
   JMeterExec,
@@ -11,56 +11,56 @@ import {
   JMeterTRunTransaction,
   JMeterRunTransaction,
   JMeterExecAnalysisReport,
-} from '../../global.type'
+} from '../../global.type';
 
 const getRunThreshold = (runName: string, thresholds: JMeterThresholds) => {
   // First use exact match
-  let runThreshold = thresholds.runs.find(t => t.name === runName)
+  let runThreshold = thresholds.runs.find((t) => t.name === runName);
   if (runThreshold !== undefined) {
-    return runThreshold
+    return runThreshold;
   }
 
   // Second, try partial match, returning the first matching record
-  runThreshold = thresholds.runs.find(t =>
+  runThreshold = thresholds.runs.find((t) =>
     runName.toLowerCase().includes(t.name.toLowerCase()),
-  )
+  );
   if (runThreshold !== undefined) {
-    return runThreshold
+    return runThreshold;
   }
 
   // Lastly, try returning "*" match or undefined if none found
-  runThreshold = thresholds.runs.find(t => t.name === '*')
-  return runThreshold
-}
+  runThreshold = thresholds.runs.find((t) => t.name === '*');
+  return runThreshold;
+};
 
 const getTransactionThreshold = (
   runRransactionName: string,
   thresholdtransactions: JMeterTRunTransaction[],
 ) => {
   let transactionThreshold = thresholdtransactions.find(
-    t => t.name === runRransactionName,
-  )
+    (t) => t.name === runRransactionName,
+  );
   if (transactionThreshold !== undefined) {
-    return transactionThreshold
+    return transactionThreshold;
   }
 
-  transactionThreshold = thresholdtransactions.find(t =>
+  transactionThreshold = thresholdtransactions.find((t) =>
     runRransactionName.toLowerCase().includes(t.name.toLowerCase()),
-  )
+  );
   if (transactionThreshold !== undefined) {
-    return transactionThreshold
+    return transactionThreshold;
   }
 
-  transactionThreshold = thresholdtransactions.find(t => t.name === '*')
-  return transactionThreshold
-}
+  transactionThreshold = thresholdtransactions.find((t) => t.name === '*');
+  return transactionThreshold;
+};
 
 class JahiaAnalyzePerfsReporter extends Command {
   static description =
     'Analyze a runs file against values included in a threshold file.';
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: 'h' }),
     runsFile: flags.string({
       description:
         'A json file containing the perf report provided by the jmeter container',
@@ -87,58 +87,58 @@ class JahiaAnalyzePerfsReporter extends Command {
   };
 
   async run() {
-    const {flags} = this.parse(JahiaAnalyzePerfsReporter)
+    const { flags } = this.parse(JahiaAnalyzePerfsReporter);
 
     if (!fs.existsSync(flags.runsFile)) {
-      this.log(`Unable to read runsFile at: ${flags.runsFile}`)
-      this.exit(1)
+      this.log(`Unable to read runsFile at: ${flags.runsFile}`);
+      this.exit(1);
     }
 
-    const rawFileJmeter = readFileSync(flags.runsFile, 'utf8')
-    const jMeterRuns: JMeterExec = JSON.parse(rawFileJmeter.toString())
+    const rawFileJmeter = readFileSync(flags.runsFile, 'utf8');
+    const jMeterRuns: JMeterExec = JSON.parse(rawFileJmeter.toString());
 
     if (!fs.existsSync(flags.thresholdsFile)) {
-      this.log(`Unable to read thresholdsFile at: ${flags.thresholdsFile}`)
-      this.exit(1)
+      this.log(`Unable to read thresholdsFile at: ${flags.thresholdsFile}`);
+      this.exit(1);
     }
 
-    const rawFileThresholds = readFileSync(flags.thresholdsFile, 'utf8')
+    const rawFileThresholds = readFileSync(flags.thresholdsFile, 'utf8');
     const jMeterThresholds: JMeterThresholds = JSON.parse(
       rawFileThresholds.toString(),
-    )
+    );
 
-    this.log('Starting analysis')
+    this.log('Starting analysis');
     this.log(
       'More details about the threshold format can be found at: https://github.com/Jahia/core-perf-test',
-    )
+    );
 
-    const analysisReport: JMeterExecAnalysisReport[] = []
+    const analysisReport: JMeterExecAnalysisReport[] = [];
     for (const run of jMeterRuns.runs) {
-      const threshold = getRunThreshold(run.name, jMeterThresholds)
+      const threshold = getRunThreshold(run.name, jMeterThresholds);
       if (threshold === undefined) {
         if (flags.verbose) {
           this.log(
             `Skipping analysis for run: ${run.name} - No threshold found`,
-          )
+          );
         }
       } else {
         this.log(
           `Analyzing run: ${run.name}, using threshold: ${threshold.name}`,
-        )
+        );
         for (let runStat of Object.values(run.statistics)) {
           // Added a check to handle new format for the statistics object used on core-perf-tests
           if (runStat.transaction === undefined) {
-            runStat = Object.values(runStat)[0]
+            runStat = Object.values(runStat)[0];
           }
           const statThreshold = getTransactionThreshold(
             runStat.transaction,
             threshold.transactions,
-          )
+          );
           if (statThreshold === undefined) {
             if (flags.verbose) {
               this.log(
                 `Skipping analysis for run: ${run.name}, transaction: ${runStat.transaction} - No threshold found`,
-              )
+              );
             }
           } else {
             for (const comp of jMeterThresholds.specs) {
@@ -148,7 +148,7 @@ class JahiaAnalyzePerfsReporter extends Command {
                 if (flags.verbose) {
                   this.log(
                     `Skipping analysis for run: ${run.name}, transaction: ${runStat.transaction} - No value for: ${comp.metric} in the transaction`,
-                  )
+                  );
                 }
               } else if (
                 statThreshold[comp.metric as keyof JMeterTRunTransaction] ===
@@ -157,45 +157,45 @@ class JahiaAnalyzePerfsReporter extends Command {
                 if (flags.verbose) {
                   this.log(
                     `Skipping analysis for run: ${run.name}, transaction: ${runStat.transaction} - No threshold found for: ${comp.metric} in the transaction`,
-                  )
+                  );
                 }
               } else {
                 const runValue =
-                  runStat[comp.metric as keyof JMeterRunTransaction]
+                  runStat[comp.metric as keyof JMeterRunTransaction];
                 const thresholdValue =
-                  statThreshold[comp.metric as keyof JMeterTRunTransaction]
-                let thresholdError = false
+                  statThreshold[comp.metric as keyof JMeterTRunTransaction];
+                let thresholdError = false;
                 if (thresholdValue !== undefined && comp.comparator === 'gt') {
                   if (runValue > thresholdValue) {
-                    thresholdError = true
+                    thresholdError = true;
                   }
                 } else if (
                   thresholdValue !== undefined &&
                   comp.comparator === 'gte'
                 ) {
                   if (runValue >= thresholdValue) {
-                    thresholdError = true
+                    thresholdError = true;
                   }
                 } else if (
                   thresholdValue !== undefined &&
                   comp.comparator === 'lt'
                 ) {
                   if (runValue < thresholdValue) {
-                    thresholdError = true
+                    thresholdError = true;
                   }
                 } else if (
                   thresholdValue !== undefined &&
                   comp.comparator === 'lte'
                 ) {
                   if (runValue <= thresholdValue) {
-                    thresholdError = true
+                    thresholdError = true;
                   }
                 }
                 if (thresholdError) {
                   if (flags.verbose) {
                     this.log(
                       `ERROR: run: ${run.name}, transaction: ${runStat.transaction}, metric: ${comp.metric} is failing threshold => Value: ${runValue} (Operator: ${comp.comparator}) Threshold: ${thresholdValue}`,
-                    )
+                    );
                   }
                   analysisReport.push({
                     error: true,
@@ -205,12 +205,12 @@ class JahiaAnalyzePerfsReporter extends Command {
                     comparator: comp.comparator,
                     runValue: runValue,
                     thresholdValue: thresholdValue,
-                  })
+                  });
                 } else {
                   if (flags.verbose) {
                     this.log(
                       `OK: run: ${run.name}, transaction: ${runStat.transaction}, metric: ${comp.metric} is passing threshold => Value: ${runValue} (Operator: ${comp.comparator}) Threshold: ${thresholdValue}`,
-                    )
+                    );
                   }
                   analysisReport.push({
                     error: false,
@@ -220,7 +220,7 @@ class JahiaAnalyzePerfsReporter extends Command {
                     comparator: comp.comparator,
                     runValue: runValue,
                     thresholdValue: thresholdValue,
-                  })
+                  });
                 }
               }
             }
@@ -230,7 +230,7 @@ class JahiaAnalyzePerfsReporter extends Command {
     }
 
     if (flags.reportFile !== '') {
-      this.log(`Saving report to: ${flags.reportFile}`)
+      this.log(`Saving report to: ${flags.reportFile}`);
       fs.writeFileSync(
         path.join(flags.reportFile),
         JSON.stringify({
@@ -238,12 +238,12 @@ class JahiaAnalyzePerfsReporter extends Command {
           tags: jMeterRuns.tags,
           analysis: analysisReport,
         }),
-      )
+      );
     }
 
-    if (analysisReport.filter(a => a.error === true).length > 0) {
-      this.log('The following values were failing threshold:')
-      for (const error of analysisReport.filter(a => a.error === true)) {
+    if (analysisReport.filter((a) => a.error === true).length > 0) {
+      this.log('The following values were failing threshold:');
+      for (const error of analysisReport.filter((a) => a.error === true)) {
         this.log(
           `ERROR: run: ${error.run}, transaction: ${
             error.transaction
@@ -254,11 +254,11 @@ class JahiaAnalyzePerfsReporter extends Command {
           )} (Operator: ${error.comparator}) Threshold: ${
             error.thresholdValue
           }`,
-        )
+        );
       }
       if (flags.failOnError) {
-        this.log('Exiting with exit code: 1 (failed)')
-        this.exit(1)
+        this.log('Exiting with exit code: 1 (failed)');
+        this.exit(1);
       }
     }
   }
--- src/commands/perfs/history.ts
+++ src/commands/perfs/history.ts
@@ -1,12 +1,12 @@
 /* eslint-disable complexity */
 /* eslint-disable max-depth */
-import {Command, flags} from '@oclif/command'
-import {lstatSync, readFileSync} from 'fs'
-import * as fs from 'fs'
-import * as glob from 'glob'
-import {format} from 'date-fns'
+import { Command, flags } from '@oclif/command';
+import { lstatSync, readFileSync } from 'fs';
+import * as fs from 'fs';
+import * as glob from 'glob';
+import { format } from 'date-fns';
 
-import {cli} from 'cli-ux'
+import { cli } from 'cli-ux';
 
 interface TransactionError {
   run: string;
@@ -28,7 +28,7 @@ class JahiaAnalyzePerfsReporter extends Command {
   static description = 'Provide an historical view over multiple run analysis';
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: 'h' }),
     analysisPath: flags.string({
       description:
         'A json file containing the perf report provided by the jmeter container',
@@ -48,55 +48,55 @@ class JahiaAnalyzePerfsReporter extends Command {
   };
 
   async run() {
-    const {flags} = this.parse(JahiaAnalyzePerfsReporter)
+    const { flags } = this.parse(JahiaAnalyzePerfsReporter);
 
     if (
       !fs.existsSync(flags.analysisPath) ||
       !lstatSync(flags.analysisPath).isDirectory()
     ) {
-      this.log(`Unable to find the following folder: ${flags.analysisPath}`)
-      this.exit(1)
+      this.log(`Unable to find the following folder: ${flags.analysisPath}`);
+      this.exit(1);
     }
 
-    const reportFiles = glob.sync(flags.analysisPath + '/**/*.json', {})
+    const reportFiles = glob.sync(flags.analysisPath + '/**/*.json', {});
 
-    const reportWindow = reportFiles.sort().slice(-flags.analysisWindow)
+    const reportWindow = reportFiles.sort().slice(-flags.analysisWindow);
 
-    const reports: Array<any> = []
+    const reports: Array<any> = [];
     // Load all report files in memory
     for (const f of reportWindow) {
-      const rawFile = readFileSync(f, 'utf8')
-      const jsonReport = JSON.parse(rawFile)
-      reports.push(jsonReport)
+      const rawFile = readFileSync(f, 'utf8');
+      const jsonReport = JSON.parse(rawFile);
+      reports.push(jsonReport);
     }
 
     // Create an array of all transactions with errors in the report
-    const errors: Array<TransactionError> = []
-    const runs: Array<string> = []
+    const errors: Array<TransactionError> = [];
+    const runs: Array<string> = [];
     for (const r of reports) {
       for (const a of r.analysis.filter(
         (a: ReportAnalysis) => a.error === true,
       )) {
         if (!runs.includes(a.run)) {
-          runs.push(a.run)
+          runs.push(a.run);
         }
         if (
           errors.find(
-            e => e.transaction === a.transaction && e.run === a.run,
+            (e) => e.transaction === a.transaction && e.run === a.run,
           ) === undefined
         ) {
           errors.push({
             run: a.run,
             transaction: a.transaction,
             metric: a.metric,
-          })
+          });
         }
       }
     }
 
     // Create a table of all values across the specified window
-    const errorsTable: Array<any> = errors.map(e => {
-      const transactions = reports.map(r => {
+    const errorsTable: Array<any> = errors.map((e) => {
+      const transactions = reports.map((r) => {
         return {
           startedAt: r.startedAt,
           analysis: r.analysis.find(
@@ -105,69 +105,69 @@ class JahiaAnalyzePerfsReporter extends Command {
               e.metric === a.metric &&
               e.run === a.run,
           ),
-        }
-      })
+        };
+      });
       return {
         ...e,
         analysis: transactions,
-      }
-    })
+      };
+    });
 
     // Format the data to be displayed in oclif table (https://oclif.io/docs/table)
     // A bit of dirty data wrangling
-    let triggerFailure = false
+    let triggerFailure = false;
     for (const run of runs) {
-      this.log(`Displaying errors for run: ${run}`)
+      this.log(`Displaying errors for run: ${run}`);
       // Each run gets its own table
-      const currentTransations = errorsTable.filter(e => e.run === run)
+      const currentTransations = errorsTable.filter((e) => e.run === run);
       const columns: any = {
         // oclif table configuration
         transaction: {},
         metric: {},
-      }
+      };
       // Add one column per report
       for (const a of currentTransations[0].analysis) {
-        const currentDate = format(new Date(a.startedAt), 'MM/DD-HH:mm')
-        columns[currentDate] = {}
+        const currentDate = format(new Date(a.startedAt), 'MM/DD-HH:mm');
+        columns[currentDate] = {};
       }
-      columns['Send Alert'] = {}
-      columns.threshold = {}
+      columns['Send Alert'] = {};
+      columns['threshold'] = {};
 
       const formattedTable = errorsTable
-      .filter(e => e.run === run)
-      .map(e => {
-        const row: any = {
-          // oclif table row
-          transaction: e.transaction,
-          metric: e.metric,
-        }
-        for (const a of e.analysis) {
-          const cellValue = Math.round(a.analysis.runValue)
-          const currentDate = format(new Date(a.startedAt), 'MM/DD-HH:mm')
-          row[currentDate] =
+        .filter((e) => e.run === run)
+        .map((e) => {
+          const row: any = {
+            // oclif table row
+            transaction: e.transaction,
+            metric: e.metric,
+          };
+          for (const a of e.analysis) {
+            const cellValue = Math.round(a.analysis.runValue);
+            const currentDate = format(new Date(a.startedAt), 'MM/DD-HH:mm');
+            row[currentDate] =
               a.analysis.error === true ? `${cellValue}*` : cellValue
-        }
-        const alertWindow = e.analysis.slice(-flags.analysisFailureAlert)
-        const alertCount = alertWindow.reduce(
-          (acc: number, a: any) => acc + (a.analysis.error === true ? 1 : 0),
-          0,
-        )
-        if (alertCount === flags.analysisFailureAlert) {
-          triggerFailure = true
-        }
-        row.threshold =
-            e.analysis[e.analysis.length - 1].analysis.thresholdValue
-        row['Send Alert'] =
-            alertCount === flags.analysisFailureAlert ? 'YES' : 'NO'
-        return row
-      })
-      this.log('* (Above threshold)')
-      cli.table(formattedTable, columns)
+          }
+          const alertWindow = e.analysis.slice(-flags.analysisFailureAlert);
+          const alertCount = alertWindow.reduce(
+            (acc: number, a: any) => acc + (a.analysis.error === true ? 1 : 0),
+            0,
+          );
+          if (alertCount === flags.analysisFailureAlert) {
+            triggerFailure = true;
+          }
+          row['threshold'] =
+            e.analysis[e.analysis.length - 1].analysis.thresholdValue;
+          row['Send Alert'] =
+            alertCount === flags.analysisFailureAlert ? 'YES' : 'NO';
+          return row;
+        });
+      this.log('* (Above threshold)');
+      cli.table(formattedTable, columns);
     }
 
     if (triggerFailure) {
-      this.log('Exiting with exit code: 1 (failed)')
-      this.exit(1)
+      this.log('Exiting with exit code: 1 (failed)');
+      this.exit(1);
     }
   }
 }
