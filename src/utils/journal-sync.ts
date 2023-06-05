f3ca3272301f579e402b1acdc5a72f5cbd782b7c
Francois G <fgerthoffert@jahia.com>
2023-06-05 10:43:25 +0200

    Display journal log output

--- src/utils/journal-sync.ts
+++ src/utils/journal-sync.ts
@@ -9,6 +9,7 @@ export const waitForJournalSync = (timeout: number, jahiaUrl: string, jahiaUsern
     .addHeader('referer', jahiaUrl)
     .addHeader('authorization', `Basic ${Base64.btoa(jahiaUsername + ':' + jahiaPassword)}`)
     .post(jahiaUrl + 'modules/graphql', {query: 'query { admin { cluster { journal { globalRevision localRevision { revision serverId } revisions { revision serverId } isClusterSync } isActivated } }}'})
+    console.log(response.data)
     if (response.errors !== undefined) break
     if (response.data === null) break
     if (response.data.admin.cluster.isActivated === undefined || response.data.admin.cluster.isActivated === false) break
