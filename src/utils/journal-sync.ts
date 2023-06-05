108ec9ee0580574d2cbb6cbc617e726e01b8af8c
Francois G <fgerthoffert@jahia.com>
2023-06-05 11:27:42 +0200

    Fixed incorrect variable name

--- src/utils/journal-sync.ts
+++ src/utils/journal-sync.ts
@@ -9,12 +9,10 @@ export const waitForJournalSync = (timeout: number, jahiaUrl: string, jahiaUsern
     .addHeader('referer', jahiaUrl)
     .addHeader('authorization', `Basic ${Base64.btoa(jahiaUsername + ':' + jahiaPassword)}`)
     .post(jahiaUrl + 'modules/graphql', {query: 'query { admin { cluster { journal { globalRevision localRevision { revision serverId } revisions { revision serverId } isClusterSync } isActivated } }}'})
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(response.data))
     if (response.errors !== undefined) break
     if (response.data === null) break
     if (response.data.admin.cluster.isActivated === undefined || response.data.admin.cluster.isActivated === false) break
-    if (response.data.admin.cluster.journal.isClusterSync === true && response.data.cluster.isActivated === true) break
+    if (response.data.admin.cluster.journal.isClusterSync === true && response.data.admin.cluster.isActivated === true) break
     
     sleep(1000)
   }
